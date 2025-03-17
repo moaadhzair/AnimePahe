db3715252c235a22a11f22b5461ea31ea099ad7e
moaadhzair <92125533+moaadhzair@users.noreply.github.com>
2025-03-17 22:14:12 +0000

    hmm

--- module/animepahe.js
+++ module/animepahe.js
@@ -1,6 +1,6 @@
 async function searchResults(keyword) {
     const encodedKeyword = encodeURIComponent(keyword);
-    const json = await fetch(`https://anime-pahe.vercel.app/api/searchResults/wwwwwwdsa${encodedKeyword}w`);
+    const json = await fetch(`https://anime-pahe.vercel.app/api/searchResults/${encodedKeyword}`);
     const finaljson = await json.json();
     return finaljson;
 }
